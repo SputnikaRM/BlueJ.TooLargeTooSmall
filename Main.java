f00749aa2a23d2424562b1c7c3231a585ad8f1ee
sputnika <sputnika91@gmail.com>
2019-05-23 14:56:34 -0400

    TooLargeTooSmall

--- Main.java
+++ Main.java
@@ -5,29 +5,41 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Main
-{
+// import scanner and random
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main{
     // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Main
-     */
-    public Main()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static void main (String [] args) {
+        
+        // number will be between 0-10.
+        Random number = new Random ();
+        int n = number.nextInt(11);
+        int tries = 0; 
+        int guess = 0;
+        
+                // get user input
+        while (guess != n) {
+            
+        System.out.println("Guess a number between 0-10");
+        Scanner num = new Scanner(System.in);
+        guess = num.nextInt();
+        tries ++; 
+        
+        
+        if (guess > n){
+        System.out.println("The number is too large.");
+        } else if (guess < n) {
+        System.out.println("The number is too small.");
+        } else {
+        
+        System.out.println("The number is correct. You're got the answer in " + tries + " tries!");
+        
+        }
+    
+    
     }
 }
+}
+
